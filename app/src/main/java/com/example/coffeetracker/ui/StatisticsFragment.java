5650d41b420f874ed588637caf812ec3561108bf
Tuns Adrian <tunsadrian99@gmail.com>
2020-05-10 17:35:44 +0300

    Configured the graph chart for productivtiy measurement

--- app/src/main/java/com/example/coffeetracker/ui/StatisticsFragment.java
+++ app/src/main/java/com/example/coffeetracker/ui/StatisticsFragment.java
@@ -8,16 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.anychart.APIlib;
-import com.anychart.AnyChart;
-import com.anychart.AnyChartView;
-import com.anychart.chart.common.dataentry.DataEntry;
-import com.anychart.chart.common.dataentry.ValueDataEntry;
-import com.anychart.charts.Pie;
 import com.example.coffeetracker.R;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.model.Viewport;
+import lecho.lib.hellocharts.view.LineChartView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -70,25 +72,87 @@ public class StatisticsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_statistics, container, false);
-
-
-        // Any Chart pie example
-
-        AnyChartView anyChartView = view.findViewById(R.id.any_chart_view);
-        APIlib.getInstance().setActiveAnyChartView(anyChartView);
-        Pie pie = AnyChart.pie();
-
-        List<DataEntry> data = new ArrayList<>();
-        data.add(new ValueDataEntry("Small Coffee", 100));
-        data.add(new ValueDataEntry("Huge Coffee", 120));
-        data.add(new ValueDataEntry("Tasty Coffee", 180));
-
-        pie.data(data);
-        pie.background().fill("#302B2D");
-
-        anyChartView.setChart(pie);
+        createActivityLevelChart(view);
 
         // Inflate the layout for this fragment
         return view;
+    }
+
+    private void createActivityLevelChart(View v) {
+
+        final int NR_DAYS_SEEN_ON_SCREEN = 7;
+
+        //TODO Astea tre sa fie trimise din DB. yAxisData o sa fie notele de productivitate si cofeeConsumed e nr de cafele baute
+        String[] xAxisData = {"May 4", "May 5", "May 6", "May 7", "May 8", "May 9", "May 10", "May 11", "May 12", "May 13", "May 14", "May 15",
+                "May 16", "MAy 17", "May 18", "May 19", "May 20", "May 21", "May 22", "May 23", "May 24", "May 25", "May 26",
+                "May 27", "May 28", "May 30", "May 31", "Jun 1", "Jun 2", "Jun 3"};
+        int[] yAxisData = {1, 2, 3, 5, 4, 2, 5, 1, 2, 3, 5, 4, 2, 5, 5, 4, 2, 3, 5, 4, 2, 1, 2, 2, 5, 2, 1, 5, 3, 4};
+        int[] coffeeConsumed = {2, 3, 1, 5, 4, 2, 4, 2, 3, 1, 5, 4, 2, 4, 6, 1, 6, 5, 3, 4, 4, 6, 5, 3, 4, 3, 5, 3, 2, 5};
+
+        LineChartView lineChartView = v.findViewById(R.id.frag_activity_level_chart);
+
+        List<AxisValue> xAxisValues = new ArrayList<>();
+        List<PointValue> yAxisValues = new ArrayList<>();
+
+        // Set line color with our Secondary Color and labels for the selected points
+        Line line = new Line(yAxisValues)
+                .setColor(this.getResources().getColor(R.color.colorSecondary))
+                .setHasLabels(true)
+                .setHasLabelsOnlyForSelected(true);
+
+        // Populate xAxisValues array
+        for (int i = 0; i < xAxisData.length; i++) {
+            xAxisValues.add(i, new AxisValue(i).setLabel(xAxisData[i]));
+        }
+
+        // Populate yAxisValues array and set each point's labels
+        for (int i = 0; i < yAxisData.length; i++) {
+            yAxisValues.add(new PointValue(i, yAxisData[i]).setLabel(String.valueOf(coffeeConsumed[i])));
+        }
+
+        // This list holds the line of the graph chart
+        List<Line> lines = new ArrayList<>();
+        lines.add(line);
+
+        // The graph line is then added to the overall data chart
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        // Populate xAxis and link it to data's bottom x Axis
+        Axis xAxis = new Axis();
+        xAxis.setValues(xAxisValues);
+        xAxis.setTextSize(16);
+        xAxis.setTextColor(this.getResources().getColor(R.color.colorAccent));
+        data.setAxisXBottom(xAxis);
+
+        // This section is used to put only whole labels on the y Axis, because by default is puts several floats between the values
+        List<AxisValue> yAxisValuesWithLabels = new ArrayList<>();  //it's Y axis value list
+        for (int i = 0; i < yAxisData.length; i++)   //WEIGHT_MAX is the max of Y values
+            yAxisValuesWithLabels.add(new AxisValue(i).setLabel(i + ""));
+
+        // Populate yAxis and link it to data's left y Axis
+        Axis yAxis = new Axis(yAxisValuesWithLabels);
+        yAxis.setName("Productivity level");
+        yAxis.setTextColor(this.getResources().getColor(R.color.colorAccent));
+        yAxis.setTextSize(16);
+        data.setAxisYLeft(yAxis);
+
+        // Add the line chat data to the view and take a viewport reference
+        lineChartView.setLineChartData(data);
+        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+
+        // Top is set with that value to prevent it from showing in the top of the labels. Without it, the top would be 5 and it wouldn't fit properly in the screen
+        viewport.top = (float) 5.1;
+        viewport.bottom = 1;
+        lineChartView.setMaximumViewport(viewport);
+        lineChartView.setZoomEnabled(false);
+
+        // Set left and right as length of dates and length minus a number of days
+        viewport.left = xAxisData.length - NR_DAYS_SEEN_ON_SCREEN;
+        viewport.right = xAxisData.length;
+
+        // Update the lineChartView
+        lineChartView.setCurrentViewport(viewport);
+        lineChartView.setViewportCalculationEnabled(false);
     }
 }
